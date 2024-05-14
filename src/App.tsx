3db30760996bafc5acd135a9ff2085387a639ddb
mquis621 <95150608+mquis621@users.noreply.github.com>
2024-05-13 21:56:02 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -378,7 +378,7 @@ function App() {
             <Settings />
           </button>
           <h1 className="flex-1 text-center text-xl xxs:text-2xl sm:text-4xl tracking-wide font-bold font-righteous">
-            WORD MASTER3
+            WORD MASTER4
           </h1>
           <button
             type="button"
